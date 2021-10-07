f2805e5aac518c967659d4d1fb4106b071f52a80
Amirhosein Barati <amirhoseinbarati75@gmail.com>
2021-10-07 19:20:29 +0330

    fixing push big json logs

--- relog/src/main/java/ir/am3n/relog/data/local/db/LogDao.kt
+++ relog/src/main/java/ir/am3n/relog/data/local/db/LogDao.kt
@@ -1,16 +1,12 @@
 package ir.am3n.relog.data.local.db
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import ir.am3n.needtool.db.BaseDao
 
 @Dao
 abstract class LogDao : BaseDao<Log>() {
 
-    @get:Query("SELECT * FROM Log LIMIT 100")
+    @get:Query("SELECT * FROM Log LIMIT 500")
     abstract val chunk: List<Log>?
-
-    @Query("SELECT * FROM Log")
-    abstract fun all(): LiveData<List<Log>?>
 
 }
